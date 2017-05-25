4bf4d15fdfb0f0ae41488dca747e3da1fee27451
nikhilmathew <nikhillukemathew@gmail.com>
2017-05-25 12:46:08 +0530

    made events work in angular

--- sfs-testing/src/app/app.service.ts
+++ sfs-testing/src/app/app.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, OnInit } from "@angular/core";
-declare var $: any;
-declare var SFS2X: any;
+declare var window: any; // for event listeners
+declare var SFS2X: any;// sfs2x object from api.js
 // import {SFS2X,SmartFox} from '../assets/sfs2x-api-1.7.3.js'
 @Injectable()
 export class AppService {
@@ -17,13 +17,13 @@ export class AppService {
         config.debug = true;
         this.sfs = new SFS2X.SmartFox(config);
         console.log(this)
-
+        console.log(window)
 
         console.log(this.sfs)
         console.log(this.sfs.isConnected);
 
 
-        this.sfs.addEventListener(SFS2X.SFSEvent.CONNECTION, onConnection, this.sfs);
+        this.sfs.addEventListener(SFS2X.SFSEvent.CONNECTION, onConnection,window);
         function onConnection(evtParams) {
             if (evtParams.success)
                 console.log("Connected to SmartFoxServer 2X!");
@@ -39,16 +39,10 @@ export class AppService {
     }
     tryARoomRequest() {
         var settings = new SFS2X.RoomSettings("My Chat Room");
-<<<<<<< HEAD
         settings.maxUsers = 20;
         settings.groupId = "chats";
         this.sfs.send(new SFS2X.CreateRoomRequest(settings));
         this.sfs.send(new SFS2X.JoinRoomRequest("The Lobby"));
 
-=======
-        settings.maxUsers = 2;
-        settings.groupId = "chats";
-        this.sfs.send(new SFS2X.CreateRoomRequest(settings));
->>>>>>> 1d0a8e3de957e909767afb106c03125b20897ff8
     }
 }
